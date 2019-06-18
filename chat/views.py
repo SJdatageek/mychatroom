8bb32655db7315bbf0601ac255ce4f45bcc3cb99
saloni jain <38145877+SJdatageek@users.noreply.github.com>
2019-06-19 01:18:58 +0530

    Update views.py

--- chat/views.py
+++ chat/views.py
@@ -9,3 +9,28 @@ def room(request, room_name):
     return render(request, 'chat/room.html', {
         'room_name_json': mark_safe(json.dumps(room_name))
     })
+
+import paralleldots
+
+
+def do_sentiment_analysis(request):
+    user_sent = ""
+    user_input = ""
+    fname = "na"
+    if request.POST:
+        user_input = request.POST.get('user_input', '')
+        lang_code = "en"
+        paralleldots.set_api_key("XXXXXXXXX")
+        user_response = paralleldots.sentiment(user_input, lang_code)
+        user_sent = user_response['sentiment']
+
+        if (user_sent == 'neutral'):
+            fname = "emoticon-1634586_640.png"
+        elif (user_sent == 'negative'):
+            fname = "emoticon-1634515_640.png"
+        elif (user_sent == 'positive'):
+            fname = "smiley-163510_640.jpg"
+        else:
+            fname = "na"
+
+    return render(request, 'my_template_img.html', {'resp': user_sent, 'fname': fname, 'user_input': user_input})
