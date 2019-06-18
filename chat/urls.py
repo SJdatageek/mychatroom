377356601ace4edca22077a08e9d73221f86d408
saloni jain <38145877+SJdatageek@users.noreply.github.com>
2019-06-19 01:18:02 +0530

    Update urls.py

--- chat/urls.py
+++ chat/urls.py
@@ -1,8 +1,14 @@
 from django.conf.urls import url
 
+from chat.views import do_sentiment_analysis
 from . import views
+
+
+
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^(?P<room_name>[^/]+)/$', views.room, name='room'),
+    #url(r'^press_my_buttons/$', press_my_buttons),
+    url(r'^do_sentiment_analysis/$', do_sentiment_analysis),
 ]
