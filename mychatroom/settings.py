7ad7356f2159f5836cd1225b4dc9e8ecd01c04f0
saloni jain <38145877+SJdatageek@users.noreply.github.com>
2019-06-19 13:01:37 +0530

    Update settings.py

--- mychatroom/settings.py
+++ mychatroom/settings.py
@@ -94,9 +94,11 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'TEST': {
+            'NAME': os.path.join(BASE_DIR, 'db_test.sqlite3')
+        }
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/2.2/ref/settings/#auth-password-validators
